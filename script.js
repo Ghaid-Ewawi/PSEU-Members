cf6a2b73dd5c805518439693433b62a9b19648a3
GhaidEwawi <ghaid_99@hotmail.com>
2019-10-12 12:06:30 +0300

    dynamically adding members

--- script.js
+++ script.js
@@ -7,20 +7,21 @@ const emailInput = document.querySelector('input[name="email"');
 const majorSelectList = document.querySelector('select[name=major]');
 const roleSelectList = document.querySelector('select[name=role]');
 
+membersContainer.innerHTML = "";
 
 let membersList = [{
     name: "Faris Abu3ram",
     email: "faris@ppu.edu.ps",
     major: "Information Technology",
     role: "Front-End Developer",
-    biography: `There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some 
-    form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure 
+    biography: `There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some` 
+    + `form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure 
     there isn't anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the 
     first true generator on the Internet. It uses a dictionary of over 200 Latin words, combined with a handful of model sentence structures, to generate Lorem 
     Ipsum which looks reasonable. The generated Lorem Ipsum is therefore always free from repetition, injected humour, or non-characteristic words etc.`
 }, {
     name: "Ibrahim Abusamra",
-    email: "faris@ppu.edu.ps",
+    email: "ibrahim@ppu.edu.ps",
     major: "Information Technology",
     role: "Front-End Developer",
     biography: `There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some 
@@ -30,7 +31,7 @@ let membersList = [{
     Ipsum which looks reasonable. The generated Lorem Ipsum is therefore always free from repetition, injected humour, or non-characteristic words etc.`
 }, {
     name: "Mohamd Alnatsha",
-    email: "faris@ppu.edu.ps",
+    email: "mohamd@ppu.edu.ps",
     major: "Information Technology",
     role: "Front-End Developer",
     biography: `There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some 
@@ -42,16 +43,18 @@ let membersList = [{
 
 membersList.forEach(member => {
 
-    membersContainer.innerHTML = "";
     let elementData  = memberTemplate.content.querySelector('.element');
+    console.log(elementData)
     let currentMember = elementData.cloneNode(true)
     currentMember.querySelector('.member-name').innerText = member.name;
     currentMember.querySelector('.email').innerText = member.email;
     currentMember.querySelector('.major').innerText = member.major;
     currentMember.querySelector('.role').innerText = member.role;
     currentMember.querySelector('.member-details').innerText = member.biography;
+    console.log(currentMember);
     membersContainer.appendChild(currentMember);
-
+    console.log(elementData)
+    console.log(member.name);
 })
 
 
